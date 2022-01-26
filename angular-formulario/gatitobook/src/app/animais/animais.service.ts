38984924e0a54bee23464ea7fcc87c4a2f72f2bf
Your Name <geminialves@outlook.com>
2022-01-25 22:52:14 -0300

    Concluída a lista de animais

--- angular-formulario/gatitobook/src/app/animais/animais.service.ts
+++ angular-formulario/gatitobook/src/app/animais/animais.service.ts
@@ -1,20 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../autenticacao/token.service';
 import { Animais } from './animais';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 
 const API = environment.apiURL;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
-
-  listaDoUsuario(nomeDoUsuario: string): Observable<Animais>{
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
     const token = this.tokenService.retornaToken();
     const headers = new HttpHeaders().append('x-access-token', token);
     return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
--- angular-formulario/gatitobook/src/app/animais/animal/animal.component.ts
+++ angular-formulario/gatitobook/src/app/animais/animal/animal.component.ts
@@ -1,34 +1,31 @@
-import { environment } from 'src/environments/environment';
 import { Component, Input, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 const API = environment.apiURL;
 
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
+  styleUrls: ['./animal.component.css'],
 })
 export class AnimalComponent implements OnInit {
-
   private urlOriginal = '';
 
   @Input() descricao = '';
 
-  @Input() set url(url:string){
-    if(url.startsWith('data')){
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
       this.urlOriginal = url;
-    }else{
-      this.url = `${API}/imgs/${url}`;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
     }
   }
 
-  get url(): string{
-    return this.urlOriginal
+  get url(): string {
+    return this.urlOriginal;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
